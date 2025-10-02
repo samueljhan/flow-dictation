8aa93338efb0e67cbfff1fd1a110aa3a2041216a
samueljhan <samueljhan@gmail.com>
2025-10-01 21:29:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080
 
 app.use(cors());
 app.use(express.json());
